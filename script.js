a0e094c649c7ffd9cc7d85238f6f10ac291b8a4e
Tushar Sharma <128169848+tushar4455@users.noreply.github.com>
2025-02-20 00:10:24 +0530

    add new

--- script.js
+++
@@ -1,7 +0,0 @@
-// JavaScript for mobile navigation toggle
-const toggleButton = document.getElementById('toggle-button');
-const navLinks = document.querySelector('.nav-links');
-
-toggleButton.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
